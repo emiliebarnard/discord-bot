0f760446c02a037190f9bf5944bcb27b6a3a0660
Emilie Menard Barnard <emiliebarnard@gmail.com>
2022-12-05 22:35:48 -0800

    sample python code working

--- bot testing/hello_world_discord_bot.py
+++ bot testing/hello_world_discord_bot.py
@@ -8,18 +8,19 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    #outputs to local command line
-    print("The bot has logged in!")
+    print("The bot has logged in!") #outputs to local command line
+    server = client.guilds[0]
+    first_channel = server.text_channels[0]
+    await first_channel.send("Hello!")
 
-@client.event
-async def on_message(message):
-    # server Hello, World! message
-    if message.content == "hello":
-        await message.channel.send("Hello, World!")
+# @client.event
+# async def on_message(message):
+#     # server Hello, World! message
+#     if message.content == "hello":
+#         await message.channel.send("Hello, World!")
 
 client.run(TOKEN)
